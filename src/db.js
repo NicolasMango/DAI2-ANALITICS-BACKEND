95fa83be2f26b5ccd5196a13a83271e0dfa91842
Nicolas Mango <nicolas_mango@hotmail.com>
2024-10-23 20:18:23 -0300

    dabasename for aws

--- src/db.js
+++ src/db.js
@@ -4,7 +4,7 @@ const { Pool } = pkg;
 const pool = new Pool({
   user: 'postgres',
   host: 'database-1.cagbs4kerxaw.us-east-1.rds.amazonaws.com',
-  database: 'yourdatabase',
+  database: 'postgres',
   password: 'TpVi3rnes0224!',
   port: 5432,
 });

a41d75b74fc4e4c7bc7acedb9b68173d4e4a22fa
Nicolas Mango <nicolas_mango@hotmail.com>
2024-10-24 20:47:00 -0300

    added swagger autogen

--- src/index.js
+++ src/index.js
@@ -28,6 +28,11 @@ app.use('/eventos', eventoRoutes);
 app.use('/compradores', compradorRoutes);
 app.use('/predecir' , predecirRoutes);
 
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger-output.json' assert {type :"json"};
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 
 app.listen(PORT, () => {
   console.log("Server is running on port", PORT);
